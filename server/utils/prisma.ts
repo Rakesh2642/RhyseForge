25aebbc2dc3fa40bf7699f7f29e2923f428ee8fb
Rakesh <Rakeshreddy2642@gmail.com>
2026-04-21 23:56:59 +0530

    Route database connection to Netlify serverless coordinates

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,8 +2,16 @@ import { PrismaClient } from '@prisma/client'
 
 let prisma: PrismaClient
 
+import path from 'path'
+
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+  const dbPath = process.env.NETLIFY ? path.join(process.cwd(), 'prisma', 'dev.db') : undefined;
+  
+  prisma = new PrismaClient(dbPath ? {
+    datasources: {
+      db: { url: `file:${dbPath}` }
+    }
+  } : undefined)
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
