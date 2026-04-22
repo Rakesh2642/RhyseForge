5217303c596d849103116050ac023bcc8d145a37
Rakesh <Rakeshreddy2642@gmail.com>
2026-04-22 22:11:49 +0530

    db

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    preset: 'netlify'
+  },
   modules: [
     '@nuxt/ui',
     '@pinia/nuxt',
