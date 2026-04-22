8c7088b9f527b8725279f16aa222620f4ef17d5c
Rakesh <Rakeshreddy2642@gmail.com>
2026-04-22 21:46:18 +0530

    build file

--- server/api/admin/questions/[id]/approve.post.ts
+++ server/api/admin/questions/[id]/approve.post.ts
@@ -1,7 +1,5 @@
 import { defineEventHandler, getRouterParam, createError } from 'h3'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '~/server/utils/prisma'
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
--- server/api/sessions/[id].patch.ts
+++ server/api/sessions/[id].patch.ts
@@ -1,7 +1,5 @@
 import { defineEventHandler, getRouterParam, readBody, createError } from 'h3'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '~/server/utils/prisma'
 
 export default defineEventHandler(async (event) => {
   const sessionId = getRouterParam(event, 'id')
